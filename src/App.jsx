737776788432d283bcf2ff37de15d5cadb73c2cb
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-06-18 15:29:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+    // .then ( data => console.log (data))
+
+  }, [])
+
+  const handelAddUser = event => {
+    event.preventDefault ();
+    const form = event.target;
+    const name = form.name.value ;
+    const email = form.email.value;
+  }
+
+  return (
+    <>
+
+    <form onSubmit={handelAddUser}>
+      <input type="text" name="name" id="" />
+      <br />
+      <input type="email" name="email" id="" />
+      <br />
+      <input type="submit" value="Ddd me" />
+    </form>
+      <h1>Module Management of the project .</h1>
+
+      <div>
+        
+        {/* {
+          users.map (user => <h1 key={user.id}>{user?.neme}</h1>)
+        } */}
+      </div>
+    </>
+  )
+}
+
+export default App
